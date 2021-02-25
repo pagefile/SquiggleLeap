4927d60c797d3641a3e79f9e0e7c237c1a1bc42e
pagefile <ahgaudette@gmail.com>
2021-02-25 10:36:46 -0600

    Script to set up platform specific stuff
    Only input right now. Probably only input ever. This is a simple game

---
+++ Assets/Scripts/System/PlatformSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSetup : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            _player.AddComponent<MobileInput>();
+        }
+        else
+        {
+            // KeyboardInput is kind of a misnomer now that I think about it but I'm too lazy to fix it right now
+            _player.AddComponent<KeyboardInput>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
